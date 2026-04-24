f08cf3f7c0421b75db243d6fbd6b95065c42e2b6
Елена Хрущ <gr632_hreev@lab-308-3.tomtit-tomsk.ru>
2026-04-24 15:14:02 +0700

    def 1

--- 2.py
+++ 2.py
@@ -1,28 +1,35 @@
 def menu():
-print("\n=== Анализатор текста ===")
-print("1. Подсчёт слов и символов")
-print("2. Частота букв")
-print("3. Самое длинное слово")
-print("4. Удалить стоп-слова")
-print("0. Выход")
+    print("\nАнализатор текста")
+    print("1. Подсчёт слов и символов")
+    print("2. Частота букв")
+    print("3. Самое длинное слово")
+    print("4. Средняя длина слова")
+
+def count_words_and_chars(text):
+    words = text.split()
+    num_words = len(words)
+    chars_with_spaces = len(text)
+    chars_without_spaces = len(text.replace(" ", ""))
+    return num_words, chars_with_spaces, chars_without_spaces
 
 def main():
-while True:
-menu()
-choice = input("Выберите опцию: ")
-if choice == "0":
-break
-text = input("Введите текст: ")
-if choice == "1":
-print("[Заглушка] Функция 1")
-elif choice == "2":
-print("[Заглушка] Функция 2")
-elif choice == "3":
-print("[Заглушка] Функция 3")
-elif choice == "4":
-print("[Заглушка] Функция 4")
-else:
-print("Неверный ввод")
+    while True:
+        text = input("Введите текст: ")
+        menu()
+        choice = input("Выберите опцию: ")
+        if choice == "1":
+            w, cws, cwos = count_words_and_chars(text)
+            print(f"Слов: {w}")
+            print(f"Символов (с побелами): {cws}")
+            print(f"Символов (без пробелов): {cwos}")
+        elif choice == "2":
+            print("Функция 2")
+        elif choice == "3":
+            print("Функция 3")
+        elif choice == "4":
+            print("Функция 4")
+        else:
+            break
 
 if __name__ == "__main__":
-main()
+    main()
